18457a689dbcd50434e87e0dede6ba9c80c488f3
Elgrek0 <gaspar093@hotmail.com>
2014-05-02 03:02:29 +0300

    Simple math circle hit

---
+++ Assets/Code/Libaries/SimpleMath/DetectHitInRadious.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectHits  {
+
+	bool DetectHitInCircle(float maxradius,float minradius,Vector2 xztarget,Vector2 xzattack){
+		float distance=sqrt(Mathf.Pow(xztarget(0)-xzattack(0),2)+Mathf.Pow(xztarget(1)-xzattack(1),2));
+		if(distance<maxradius&&distance>miminradius)
+			return(true);
+		else
+			return(false);
+	}
+
+}
