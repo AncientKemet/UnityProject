159ed2ef7246bf47a20c915d61346a8bb8c17dde
Domco Kollar <tollhotep@gmail.com>
2014-05-04 16:01:04 +0200

    no message

---
+++ Assets/Code/Scripts/PlayAnimScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Animator anim = GetComponent<Animator> ();
+		anim.Play(0);
+	}
+
+}
