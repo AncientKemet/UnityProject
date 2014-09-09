717d136532361d8a41bfd16af787520d0fe27b5a
Robert Kollar <robco.kollar@gmail.com>
2014-09-09 19:03:28 +0200

    no message

--- Assets/Editor/OldBloodEditor.cs
+++ Assets/Editor/OldBloodEditor.cs
@@ -3,68 +3,75 @@ using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using System.Collections;
 
-public class OldBloodEditor
+namespace Assets.Editor
 {
-
-    private static string BeginFileMacro = "#if SERVER";
-    private static string EndFileMacro = "#if SERVER";
-
-    [MenuItem("OldBlood/ServerFiles")]
-    private static void Do()
+    /// <summary>
+    /// Makes sure that all server cs files are marcoed  
+    /// </summary>
+    public class OldBloodEditor
     {
-        List<string> Directories = new List<string>();
-        List<string> Files = new List<string>();
-        GetDirectories(Application.dataPath+"/Code/Core/Server", Directories);
+
+        private static string BeginFileMacro = "#if SERVER";
+        private static string EndFileMacro = "#endif";
+
+        [MenuItem("OldBlood/ServerFiles")]
+        private static void Do()
+        {
+            List<string> Directories = new List<string>();
+            List<string> Files = new List<string>();
+            GetDirectories(Application.dataPath+"/Code/Core/Server", Directories);
         
-        foreach (var dir in Directories)
-        {
-            GetCss(dir, Files);
+            foreach (var dir in Directories)
+            {
+                GetCss(dir, Files);
+            }
+
+            foreach (var file in Files)
+            {
+                EnsureMacro(file);
+            }
         }
 
-        foreach (var file in Files)
+        private static void EnsureMacro(string file)
         {
-            EnsureMacro(file);
+            string[] lines = File.ReadAllLines(file);
+
+            string firstLine = lines.First();
+            string lastLine = lines.Last();
+
+            if (firstLine != BeginFileMacro)
+            {
+                List<string> newLines = new List<string>(lines);
+                newLines.Insert(0, BeginFileMacro);
+                lines = newLines.ToArray();
+                File.WriteAllLines(file, lines);
+            }
+
+            if (lastLine != EndFileMacro)
+            {
+                List<string> newLines = new List<string>(lines);
+                newLines.Add(EndFileMacro);
+                lines = newLines.ToArray();
+                File.WriteAllLines(file, lines);
+            }
         }
-    }
 
-    private static void EnsureMacro(string file)
-    {
-        string[] lines = File.ReadAllLines(file);
-
-        string firstLine = lines.First();
-        string lastLine = lines.Last();
-
-        if (firstLine != BeginFileMacro)
+        private static void GetDirectories(string path, List<string> all)
         {
-            List<string> newLines = new List<string>(lines);
-            newLines.Insert(0, BeginFileMacro);
-            File.WriteAllLines(file, newLines.ToArray());
+            string[] dirs = Directory.GetDirectories(path, "*");
+            all.AddRange(dirs);
+
+            foreach (var directory in dirs)
+            {
+                GetDirectories(directory, all);
+            }
         }
 
-        if (lastLine != EndFileMacro)
+        private static void GetCss(string path, List<string> all)
         {
-            List<string> newLines = new List<string>(lines);
-            newLines.Add(EndFileMacro);
-            File.WriteAllLines(file, newLines.ToArray());
+            string[] files = Directory.GetFiles(path, "*.cs");
+            all.AddRange(files);
         }
-    }
-
-    private static void GetDirectories(string path, List<string> all)
-    {
-        string[] dirs = Directory.GetDirectories(path, "*");
-        all.AddRange(dirs);
-
-        foreach (var directory in dirs)
-        {
-            GetDirectories(directory, all);
-        }
-    }
-
-    private static void GetCss(string path, List<string> all)
-    {
-        string[] files = Directory.GetFiles(path, "*.cs");
-        all.AddRange(files);
     }
 }
